7674005d424f6f22630ca4111aa08c1697454949
ducanhngu <114657715+ducanhngu@users.noreply.github.com>
2022-10-13 16:32:16 +0700

    Add files via upload

---
+++ project1/footer.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+
+<h2>Copyright @ 2020 Design by Xuân Quỳnh</h2>
+ 
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
